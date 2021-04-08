412ca5faa8e1a2bd38f68c57b549878d92d8d4d2
Sean Palmer <palmersean96@outlook.com>
2021-04-07 19:42:34 -0700

    getting ready for heroku

--- RTI/settings.py
+++ RTI/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '!xw!x2qrs+@$x93un40pjn9nqzn&rbuew0*#9dmj13$rjm(n59'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rti-network-monitor.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
